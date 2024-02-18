9218972e9fbdebb4de46e53a7cbec83d828f6414
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2024-02-18 21:32:23 +0100

    Revert "fix: remove w, q, and x from keyboard"
    This reverts commit 97ce6b45251f86d6332f6a4ec6e5ba690088945f.

--- src/App.tsx
+++ src/App.tsx
@@ -258,9 +258,9 @@ export const App = (): JSX.Element => {
           ]}
           layout={{
             default: [
-              "C E Y U I O P Ğ Ü ←",
+              "Q W E R T Y U I O P Ğ Ü ←",
               "A S D F G H J K L Ş İ",
-              "Z R T V B N M Ö Ç ↵",
+              "Z X C V B N M Ö Ç ↵",
             ],
           }}
           onKeyPress={handleKeyPress}
